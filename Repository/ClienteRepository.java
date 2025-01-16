159e103c34f31900a4909fa8b08adeb535d30c8a
Emanuel Rodrigues <emanuelrodrigues112358@gmail.com>
2025-01-16 16:38:59 -0300

    fix: remove all trycatch from code

--- Repository/ClienteRepository.java
+++ Repository/ClienteRepository.java
@@ -13,99 +13,79 @@ public class ClienteRepository {
     }
 
     public void getAllClientes() {
-        try {
-            if (clientes.isEmpty()) {
-                System.out.println("\nNenhum cliente foi cadastrado!\n");
-            } else {
-                System.out.println("\nLista de clientes:\n");
-                for (Cliente cliente : clientes) {
-                    System.out.printf("Cliente: %s\n", cliente.getNome());
+        if (clientes.isEmpty()) {
+            System.out.println("\nNenhum cliente foi cadastrado!\n");
+        } else {
+            System.out.println("\nLista de clientes:\n");
+            for (Cliente cliente : clientes) {
+                System.out.printf("Cliente: %s\n", cliente.getNome());
 
-                    for (Contrato contrato : cliente.getContratos()) {
-                        System.out.printf("%s;\nSaldo Devedor: %.2f;\nStatus do Contrato: %s.\n\n", contrato.getGrupoAssociado(), contrato.getSaldoDevedor(), contrato.getStatusContrato());
-                    }
+                for (Contrato contrato : cliente.getContratos()) {
+                    System.out.printf("%s;\nSaldo Devedor: %.2f;\nStatus do Contrato: %s.\n\n", contrato.getGrupoAssociado(), contrato.getSaldoDevedor(), contrato.getStatusContrato());
                 }
             }
-        } catch (Exception e) {
-            System.out.println("Erro ao obter a lista de clientes: " + e.getMessage());
         }
     }
 
     public Cliente getClienteByCpf(String cpf) {
-        try {
-            for (Cliente cliente : clientes) {
-                if (cliente.getCpf().equals(cpf)) {
-                    System.out.println(cliente);
-                    return cliente;
-                }
+        for (Cliente cliente : clientes) {
+            if (cliente.getCpf().equals(cpf)) {
+                System.out.println(cliente);
+                return cliente;
             }
-        } catch (Exception e) {
-            System.out.println("Erro ao buscar cliente pelo CPF: " + e.getMessage());
         }
         return null;
     }
 
     public Cliente createCliente(String nome, String cpf, String telefone, String email) {
-        try {
-            if (getClienteByCpf(cpf) == null) {
-                Cliente cliente = new Cliente(nome, cpf, telefone, email);
+        if (getClienteByCpf(cpf) == null) {
+            Cliente cliente = new Cliente(nome, cpf, telefone, email);
 
-                clientes.add(cliente);
+            clientes.add(cliente);
 
-                System.out.println(cliente);
+            System.out.println(cliente);
 
-                System.out.printf("Cliente '%s' adicionado com sucesso!\n\n", cliente.getNome());
-                return cliente;
-            } else {
-                System.out.println("Cpf já cadastrado para um cliente!");
-            }
-        } catch (Exception e) {
-            System.out.println("Erro ao criar cliente: " + e.getMessage());
+            System.out.printf("Cliente '%s' adicionado com sucesso!\n\n", cliente.getNome());
+            return cliente;
+        } else {
+            System.out.println("Cpf já cadastrado para um cliente!");
         }
         return null;
     }
 
     public Cliente updateCliente(String cpf, String novoNome, String novoTelefone, String novoEmail) {
-        try {
-            Cliente clienteTemp = getClienteByCpf(cpf);
+        Cliente clienteTemp = getClienteByCpf(cpf);
 
-            if (clienteTemp != null) {
-                if (!novoNome.isEmpty()) {
-                    clienteTemp.setNome(novoNome);
-                }
-
-                if (!novoTelefone.isEmpty()) {
-                    clienteTemp.setTelefone(novoTelefone);
-                }
-
-                if (!novoEmail.isEmpty()) {
-                    clienteTemp.setEmail(novoEmail);
-                }
-
-                System.out.println(clienteTemp);
-                System.out.println("\nCliente atualizado com sucesso!\n");
-                return clienteTemp;
-            } else {
-                System.out.println("\nNenhum cliente encontrado com o CPF informado!\n");
+        if (clienteTemp != null) {
+            if (!novoNome.isEmpty()) {
+                clienteTemp.setNome(novoNome);
             }
-        } catch (Exception e) {
-            System.out.println("Erro ao atualizar cliente: " + e.getMessage());
+
+            if (!novoTelefone.isEmpty()) {
+                clienteTemp.setTelefone(novoTelefone);
+            }
+
+            if (!novoEmail.isEmpty()) {
+                clienteTemp.setEmail(novoEmail);
+            }
+
+            System.out.println(clienteTemp);
+            System.out.println("\nCliente atualizado com sucesso!\n");
+            return clienteTemp;
+        } else {
+            System.out.println("\nNenhum cliente encontrado com o CPF informado!\n");
         }
         return null;
     }
 
     public void deleteCliente(String cpf) {
-        try {
-            Cliente clienteTemp = getClienteByCpf(cpf);
+        Cliente clienteTemp = getClienteByCpf(cpf);
 
-            if (clienteTemp != null) {
-                clientes.remove(clienteTemp);
-                System.out.printf("\nCliente '%s' deletado com sucesso!\n", clienteTemp.getNome());
-            } else {
-                System.out.println("\nNenhum cliente encontrado com o CPF informado!\n");
-            }
-        } catch (Exception e) {
-            System.out.println("Erro ao deletar cliente: " + e.getMessage());
+        if (clienteTemp != null) {
+            clientes.remove(clienteTemp);
+            System.out.printf("\nCliente '%s' deletado com sucesso!\n", clienteTemp.getNome());
+        } else {
+            System.out.println("\nNenhum cliente encontrado com o CPF informado!\n");
         }
     }
 }
--- Repository/GrupoConsorcioRepository.java
+++ Repository/GrupoConsorcioRepository.java
@@ -12,49 +12,36 @@ public class GrupoConsorcioRepository {
     }
 
     public void getAllGrupos() {
-        try {
-            if (grupos.isEmpty()) {
-                System.out.println("\nNenhum grupo foi criado!\n");
-            } else {
-                System.out.println("\nLista de grupos:");
-                for (GrupoConsorcio grupo : grupos) {
-                    System.out.printf("- %s (participantes: %d)\n", grupo.getNomeGrupo(), grupo.getNumeroParticipantes());
-                }
+        if (grupos.isEmpty()) {
+            System.out.println("\nNenhum grupo foi criado!\n");
+        } else {
+            System.out.println("\nLista de grupos:");
+            for (GrupoConsorcio grupo : grupos) {
+                System.out.printf("- %s (participantes: %d)\n", grupo.getNomeGrupo(), grupo.getNumeroParticipantes());
             }
-        } catch (Exception e) {
-            System.out.println("Erro ao listar os grupos: " + e.getMessage());
         }
     }
 
     public GrupoConsorcio getGrupoByName(String nomeGrupo) {
-        try {
-            for (GrupoConsorcio grupo : grupos) {
-                if (grupo.getNomeGrupo().equalsIgnoreCase(nomeGrupo)) {
-                    System.out.println(grupo);
-                    return grupo;
-                }
+        for (GrupoConsorcio grupo : grupos) {
+            if (grupo.getNomeGrupo().equalsIgnoreCase(nomeGrupo)) {
+                System.out.println(grupo);
+                return grupo;
             }
-        } catch (Exception e) {
-            System.out.println("Erro ao buscar o grupo: " + e.getMessage());
         }
         return null;
     }
 
     public GrupoConsorcio createGrupoConsorcio(String nomeGrupo, int numeroParticipantes, double valorTotal, double taxaAdmin) {
-        try {
-            if(getGrupoByName(nomeGrupo) == null){
-                GrupoConsorcio grupoConsorcio = new GrupoConsorcio(nomeGrupo, numeroParticipantes, valorTotal, taxaAdmin);
+        if(getGrupoByName(nomeGrupo) == null){
+            GrupoConsorcio grupoConsorcio = new GrupoConsorcio(nomeGrupo, numeroParticipantes, valorTotal, taxaAdmin);
 
-                grupos.add(grupoConsorcio);
+            grupos.add(grupoConsorcio);
 
-                System.out.println(grupoConsorcio);
+            System.out.println(grupoConsorcio);
 
-                System.out.printf("Grupo '%s' criado e adicionado com sucesso!\n\n", grupoConsorcio.getNomeGrupo());
-                return grupoConsorcio;
-            }
-
-        } catch (Exception e) {
-            System.out.println("Erro ao criar o grupo: " + e.getMessage());
+            System.out.printf("Grupo '%s' criado e adicionado com sucesso!\n\n", grupoConsorcio.getNomeGrupo());
+            return grupoConsorcio;
         }
         return null;
     }
@@ -66,56 +53,48 @@ public class GrupoConsorcioRepository {
             double novoTaxaAdmin,
             StatusGrupoConsorcioEnum statusGrupoConsorcio
     ) {
-        try {
-            GrupoConsorcio grupoTemp = getGrupoByName(nomeGrupo);
+        GrupoConsorcio grupoTemp = getGrupoByName(nomeGrupo);
 
-            if (grupoTemp != null) {
-                if (novoNumeroParticipantes > 0) {
-                    grupoTemp.setNumeroParticipantes(novoNumeroParticipantes);
-                }
-
-                if (novoValorTotal > 0) {
-                    grupoTemp.setValorTotal(novoValorTotal);
-                }
-
-                if (novoTaxaAdmin >= 0) {
-                    grupoTemp.setTaxaAdmin(novoTaxaAdmin);
-                }
-
-                if (statusGrupoConsorcio != null) {
-                    grupoTemp.setStatusGrupoConsorcio(statusGrupoConsorcio);
-                }
-
-                double novoValorParcela = (novoValorTotal + (novoValorTotal * novoTaxaAdmin)) / novoNumeroParticipantes;
-
-                if (novoValorParcela > 0) {
-                    grupoTemp.setValorParcela(novoValorParcela);
-                }
-
-                System.out.println(grupoTemp);
-                System.out.printf("\nGrupo '%s' atualizado com sucesso!\n", grupoTemp.getNomeGrupo());
-                return grupoTemp;
-            } else {
-                System.out.println("Erro: Grupo não encontrado para atualização!");
+        if (grupoTemp != null) {
+            if (novoNumeroParticipantes > 0) {
+                grupoTemp.setNumeroParticipantes(novoNumeroParticipantes);
             }
-        } catch (Exception e) {
-            System.out.println("Erro ao atualizar o grupo: " + e.getMessage());
+
+            if (novoValorTotal > 0) {
+                grupoTemp.setValorTotal(novoValorTotal);
+            }
+
+            if (novoTaxaAdmin >= 0) {
+                grupoTemp.setTaxaAdmin(novoTaxaAdmin);
+            }
+
+            if (statusGrupoConsorcio != null) {
+                grupoTemp.setStatusGrupoConsorcio(statusGrupoConsorcio);
+            }
+
+            double novoValorParcela = (novoValorTotal + (novoValorTotal * novoTaxaAdmin)) / novoNumeroParticipantes;
+
+            if (novoValorParcela > 0) {
+                grupoTemp.setValorParcela(novoValorParcela);
+            }
+
+            System.out.println(grupoTemp);
+            System.out.printf("\nGrupo '%s' atualizado com sucesso!\n", grupoTemp.getNomeGrupo());
+            return grupoTemp;
+        } else {
+            System.out.println("Erro: Grupo não encontrado para atualização!");
         }
         return null;
     }
 
     public void deleteGrupoConsorcio(String nomeGrupo) {
-        try {
-            GrupoConsorcio grupoTemp = getGrupoByName(nomeGrupo);
+        GrupoConsorcio grupoTemp = getGrupoByName(nomeGrupo);
 
-            if (grupoTemp != null) {
-                grupos.remove(grupoTemp);
-                System.out.printf("Grupo '%s' deletado com sucesso!\n", grupoTemp.getNomeGrupo());
-            } else {
-                System.out.println("Grupo não encontrado para exclusão!");
-            }
-        } catch (Exception e) {
-            System.out.println("Erro ao deletar o grupo: " + e.getMessage());
+        if (grupoTemp != null) {
+            grupos.remove(grupoTemp);
+            System.out.printf("Grupo '%s' deletado com sucesso!\n", grupoTemp.getNomeGrupo());
+        } else {
+            System.out.println("Grupo não encontrado para exclusão!");
         }
     }
 }
